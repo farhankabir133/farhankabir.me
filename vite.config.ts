571d18b6f8d41c9c0c9cd41d0fdcb1852ef034b8
AutoSpark CI <farhan@autospark.bd>
2026-05-11 09:33:18 +0600

    chore: add gh-pages deployment and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/farhankabir.me/",
   plugins: [react()],
   build: {
     target: "ES2020",
